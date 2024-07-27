2e4aae23f72aaf100042c453fc536d41ee61428f
igor <igor>
2024-07-27 12:46:09 +0300

    migrating to catch3

--- tests/src/dense_hash_map_tests.cpp
+++ tests/src/dense_hash_map_tests.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch2/catch.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include "jg/dense_hash_map.hpp"
 #include "jg/details/type_traits.hpp"
 
